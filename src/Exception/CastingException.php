8861aba1896ea8ef8a0ba2832f37f6eb986cb7ee
web-fu <info@web-fu.it>
2025-12-16 18:03:15 +0100

    CS Fix

--- src/Exception/CastingException.php
+++ src/Exception/CastingException.php
@@ -1,8 +1,19 @@
 <?php
+declare(strict_types=1);
+
+/**
+ * This file is part of web-fu/simple-repository
+ *
+ * @copyright Web-Fu <info@web-fu.it>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace WebFu\SimpleRepository\Exception;
+use Exception;
 
-class CastingException extends \Exception
+class CastingException extends Exception
 {
 
 }
